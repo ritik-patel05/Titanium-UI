e055534ddcbb41bee7f18e5116af2d96267388e5
Ritik Patel <ritikptl05@gmail.com>
2022-02-13 19:00:13 +0530

    feat: add get started page

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,7 +1,46 @@
-const navbar = document.querySelector(".navbar-burger");
-const navbarMenu = document.querySelector(".navbar-menu");
+// Top Navbar toggles
 
-navbar.addEventListener("click", () => {
-    navbar.classList.toggle("is-active");
-    navbarMenu.classList.toggle("is-active");
-});
+const topNavbarToggles = () => {
+	const navbarBurger = document.getElementById("navbarBurger");
+	const navbarMenu = document.getElementById("navbarMenu");
+
+	navbarBurger.addEventListener("click", (event) => {
+		navbarBurger.classList.toggle("is-active");
+		navbarMenu.classList.toggle("is-active");
+	});
+}
+
+topNavbarToggles();
+
+// Side Navbar mobile toggles
+
+const sideNavbarToggles = () => {
+	let showNav = false;
+
+	const docs = document.getElementById('docs');
+	const overlay = document.getElementById('docsNavOverlay');
+	const nav = document.getElementById('docsNav');
+	const navButton = document.getElementById('docsNavButton');
+
+	navButton.addEventListener('click', (event) => {
+		showNav = !showNav;
+		updateUI();
+	});
+
+	overlay.addEventListener('click', (event) => {
+		showNav = false;
+		updateUI();
+	});
+
+	const updateUI = () => {
+		if (showNav) {
+			docs.classList.add('showing-overlay');
+			nav.classList.add("is-shown");
+		} else {
+			docs.classList.remove('showing-overlay');
+			nav.classList.remove('is-shown');
+		}
+	};
+}
+
+sideNavbarToggles();
